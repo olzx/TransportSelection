2609d98d4298a8df828f4c587fc38ba92618fcc1
Olzx <ol2az@yandex.ru>
2022-04-27 00:39:12 +0700

    removed debug file

--- scripts/cards/addCardsInDocument.js
+++
@@ -1,9 +0,0 @@
-import addCard from './addCard.js'
-
-// (function allVehicleLoad() {
-//     const vehiclesJson = vehicles_img_data
-//     console.log("Транспорта всего: " + vehiclesJson.length)
-//     for(const vehicle of vehiclesJson) {
-//         addCard(vehicle.name, vehiclesJson.length)
-//     }
-// })();
